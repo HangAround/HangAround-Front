6a319f6fba008a01096fc3f3321d0cf32795e23b
seungjikim <necky10000@gmail.com>
2022-03-20 02:32:37 +0900

    temp

---
+++ hangaround-front/src/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { render } from 'react-dom'
+import { StyledEngineProvider } from '@mui/material'
+import './index.css'
+import Login from './pages/Login.tsx'
+import Main from './pages/Main.tsx'
+import Room from './pages/Room.tsx'
+import './statics/fonts/index.css'
+
+const rootElement = document.getElementById('root')
+
+render(
+  <StyledEngineProvider injectFirst>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/login" element={<Login />} />
+        <Route path="/" element={<Main />} />
+        <Route path="/room" element={<Room />} />
+      </Routes>
+    </BrowserRouter>
+  </StyledEngineProvider>,
+  rootElement
+)
