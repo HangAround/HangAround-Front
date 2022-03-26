8af2faeb62bb495056fd7fcac7f1fe8233761f51
seungjikim <necky10000@gmail.com>
2022-03-27 00:34:55 +0900

    FEAT: Add onClickWithouLogin for user session storage #5

---
+++ hangaround-front/src/App.tsx
@@ -0,0 +1,21 @@
+import React, { ReactElement } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { StyledEngineProvider } from '@mui/material'
+
+import './index.css'
+import Login from './pages/Login'
+import Main from './pages/Main'
+import './statics/fonts/index.css'
+
+export default function App(): ReactElement {
+  return (
+    <StyledEngineProvider injectFirst>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </BrowserRouter>
+    </StyledEngineProvider>
+  )
+}
--- hangaround-front/src/index.tsx
+++ hangaround-front/src/index.tsx
@@ -1,23 +1,14 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React, { StrictMode } from 'react'
 import { render } from 'react-dom'
-import { StyledEngineProvider } from '@mui/material'
 
 import './index.css'
-import Login from './pages/Login.tsx'
-import Main from './pages/Main.tsx'
-import './statics/fonts/index.css'
+import App from './App'
 
 const rootElement = document.getElementById('root')
 
 render(
-  <StyledEngineProvider injectFirst>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Main />} />
-      </Routes>
-    </BrowserRouter>
-  </StyledEngineProvider>,
+  <StrictMode>
+    <App />
+  </StrictMode>,
   rootElement
 )
