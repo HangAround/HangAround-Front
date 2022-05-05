f1353232bc554e7d5e9905981f16ba2ae2929025
kim-yeji <2214yjgm@gmail.com>
2022-05-05 21:57:00 +0900

    REFACTOR: Refactor error handling

--- hangaround-front/src/pages/STTSrc.js
+++ hangaround-front/src/pages/STTSrc.js
@@ -79,25 +79,27 @@ recognition.onresult = async function (e) {
 
     speechText.textContent = texts;
 
-    //초성 일치하는지 확인하기 위해 단어로부터 초성을 분리
-    let kor1 = texts[0];
-    let uni1 = kor1.charCodeAt(0);
-    uni1 = uni1 - 44032;
-    let fn1 = parseInt(uni1 / 588);
+    try{
+        //초성 일치하는지 확인하기 위해 단어로부터 초성을 분리
+        let kor1 = texts[0];
+        let uni1 = kor1.charCodeAt(0);
+        uni1 = uni1 - 44032;
+        let fn1 = parseInt(uni1 / 588);
 
-    let kor2 = texts[1];
-    let uni2 = kor2.charCodeAt(0);
-    uni2 = uni2 - 44032;
-    let fn2 = parseInt(uni2 / 588);
+        let kor2 = texts[1];
+        let uni2 = kor2.charCodeAt(0);
+        uni2 = uni2 - 44032;
+        let fn2 = parseInt(uni2 / 588);
 
+        //랜덤 초성과 단어의 초성이 일치하는지 확인, 단어 확인 함수를 통해 사전에 있는 단어인지 확인
+        if ((consonant[first] === f[fn1]) && (consonant[second] === f[fn2]) && (await checkWord(texts.substring(0, 2)))) {
+            resultText.textContent = "정답!";
+        } else {
+            resultText.textContent = "땡!"
+        }
 
-    //랜덤 초성과 단어의 초성이 일치하는지 확인, 단어 확인 함수를 통해 사전에 있는 단어인지 확인
-    if ((consonant[first] === f[fn1]) && (consonant[second] === f[fn2]) && (await checkWord(texts.substring(0, 2)))) {
-        resultText.textContent = "정답!";
-    } else {
-        resultText.textContent = "땡!"
+        console.log(texts);
+    } catch (e) {
+        console.log("두글자 이상 입력되지 않음");
     }
-
-    console.log(texts);
 };
-
